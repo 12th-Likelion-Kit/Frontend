5a5ccea4da11c403663f71de7daf6a3f437142f7
kimche9865@naver.com <kimche9865@naver.com>
2024-05-06 22:04:20 +0900

    2

--- 과제2/main.js
+++ 과제2/main.js
@@ -1,26 +1,25 @@
 // 1. 템플릿 리터럴을 활용하여 a + b를 출력하시오
 const a = 10;
 const b = 20;
-console.log(`${a + b}`)  // 10 + 20 = 30입니다.
-
+console.log(a + b) // 10 + 20 = 30입니다.
 
 // 2. 구조 분해 할당을 활용하여 올바르게 출력할 수 있도록 코드를 작성하시오.
 const obj = { a: 10, b: 20, c: 30 };
 
-function sum ({a, b, c}) {
-	console.log(`${a} + ${b} + ${c} = ${a + b + c}`);  // 10 + 20 + 30 = 60
+function sum ({a,b,c}) {
+	console.log(`${a} + ${b} + ${c} = ${a + b + c}`);
+	// 10 + 20 + 30 = 60
 }
+
 
 sum(obj);
 
-
 // 3. spread문법을 활용하여 올바르게 출력할 수 있도록 코드를 작성하시오.
 const arr = [1, 2, 3, 4, 5];
-let [a, b, ...rest] = arr;
-console.log(a);  // 1
-console.log(b);  // 2
-console.log(rest);  // [3, 4, 5]
-
+let [a,b,...rest] = arr;
+console.log(a); // 1
+console.log(b); // 2
+console.log(rest); // [3, 4, 5]
 
 // 4. 다음 아기사자 리스트를 랜덤으로 섞은 상태에서 배열 메소드를 활용하여
 //    "황용진"을 찾아서 리스트에서 제거하고 출력하도록 코드를 작성하시오.
@@ -44,14 +43,9 @@ function shuffle(array) {
 
 shuffle(nameList);
 
-for(let i=0; i<nameList.length; i++) {
-  if(nameList[i] === "황용진") {
-    nameList.splice(i, 1);
-  }
-  console.log(nameList[i]);
-}
+namelist = nameList.filter((value) => value !="황용진");
 
-
+console.log(namelist)
 // 5. 다음 리스트를 like가 높은 순서로 정렬하여 출력하도록 하시오.
 const list = [
   {
@@ -76,14 +70,8 @@ const list = [
   },
 ];
 
-let like_high = 0;
-for(let i=0; i<list.length; i++) {
-  if(list[i].like > like_high) {
-    like_high = list[i].like;
-  }
-}
-console.log(like_high);
-
+list.sort((a,b) => a.like - b.like)
+console.log(list)
 
 // 6. 배열 메소드 map을 활용하여 제목, 내용을 순서대로 출력하도록 하시오.
 const list = [
@@ -114,9 +102,7 @@ const list = [
   },
 ];
 
-list.map(function(element) {
-  console.log(`${element.title}\n${element.content}`); 
-});
+list.map((a) => console.log(`${a.title}\n${a.content}`));
 /*
 제목1
 내용1
