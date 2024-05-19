f1ed6df3e41b3a0371db4bb8a9372830270bb2c1
hyunsik2000 <ryuhsik2000@naver.com>
2024-05-19 15:44:18 +0900

    4번 과제 제출

---
+++ RHS_4/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import dayjs from "dayjs";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h3>오늘은 📆</h3>
+      <h1>{dayjs("2024-05-19").format("YYYY-MM-DD dddd")}</h1>
+    </div>
+  );
+};
+
+export default Header;
